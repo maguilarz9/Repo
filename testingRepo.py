d02cb353535a0baba306cb488b611a0c07a53acb
maguilarz <melaguilarza@gmail.com>
2020-07-18 22:52:19 -0500

    Testing local Repo

---
+++ testingRepo.py
@@ -0,0 +1,9 @@
+whateverList= ['a',5,'b',8,'c',2]
+
+while whatever_iterator:
+    try:
+        value = next(whatever_iterator)
+        print(value)
+    except StopIteration:
+        print("End of iterator")
+        break
